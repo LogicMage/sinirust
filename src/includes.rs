af2982f4790ef003080cdd02a20f0380dbf302aa
Hubert Stumble <searcher@mailbox.org>
2026-04-12 02:06:17 -0400

    Added main menu

--- src/includes.rs
+++ src/includes.rs
@@ -9,7 +9,7 @@ pub const WORLD_WIDTH: f32 = 4000.0;
 pub const WORLD_HEIGHT: f32 = 4000.0;
 
 #[derive(Resource, Default)]
-pub struct GameScore(pub u32);
+pub struct GameScore(pub u32, pub u32);
 
 #[derive(Resource, Default)]
 pub struct Sinibombs(pub u32);
